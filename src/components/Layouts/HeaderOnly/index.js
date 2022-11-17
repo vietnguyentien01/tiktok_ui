0ca4c6996f69c2feda779b971e34f4d611113592
VietNguyenTien <vietnguyentien1808@gmail.com>
2022-11-18 03:42:46 +0700

    frame construction layout

--- src/components/Layouts/HeaderOnly/index.js
+++ src/components/Layouts/HeaderOnly/index.js
@@ -1,4 +1,4 @@
-import Header from '~/components/Layout/Components/Header';
+import Header from '~/components/Layouts/Components/Header';
 
 function HeaderOnly({ children }) {
   return (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <div>Home Page</div>;
+  return <h2>Home Page</h2>;
 }
 
 export default Home;
